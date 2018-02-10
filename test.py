a61e7216be07392de5c1c2525a18706c5d531a77
develjacob <mcjacob61@gmail.com>
2018-02-09 17:20:26 -0900

    inital test, just run

--- test.py
+++ test.py
@@ -8,9 +8,14 @@ Created 2/09/18
 '''
 
 import unittest
-from cs372_battleship import Ship
+import ship
 
 class Tests(unittest.TestCase):
 
 	def test(self):
 		print('Can I see and import my ship?')
+
+
+
+if __name__ == '__main__':
+	unittest.main()
