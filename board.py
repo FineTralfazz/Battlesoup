4b04b1f1e19e02e7361f6cfadd1bbc480cd737af
develjacob <mcjacob61@gmail.com>
2018-02-16 15:37:13 -0900

    add new is_ship_placed_on_ship

--- board.py
+++ board.py
@@ -28,11 +28,16 @@ class Board(object):
             return False
         return True
 
+    def is_ship_placed_on_ship(self):
+        
+
+        return True
+
     def is_ship_out_of_bounds(self, x, y, l, d):
         ship = Ship(x,y,l,d)
-        print('Ship pins', ship.get_pins())
+        # print('Ship pins', ship.get_pins())
         for coord in ship.get_pins():
-            print(coord[0], coord[1])
+            # print(coord[0], coord[1])
             if not self.is_valid_index(coord[0]):
                 return False
             if not self.is_valid_index(coord[1]):
@@ -51,6 +56,8 @@ class Board(object):
             return False 
         if not self.is_ship_out_of_bounds(x,y,l,d):
             return False       
+        if not self.is_ship_out_of_bounds():
+            return False
 
         return True
 
